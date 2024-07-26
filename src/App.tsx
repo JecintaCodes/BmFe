cf9d5039262d44dddcd8fe3daa9daca87db102b1
JecintaCodes <onyemaobijecintaugochi@gmail.com>
2024-07-26 13:47:51 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { RouterProvider } from "react-router-dom"
-import { mainRoute } from "./router/mainRoute"
-import { Provider } from "react-redux"
-import { store } from "./components/global/store"
+import { RouterProvider } from "react-router-dom";
+import { mainRoute } from "./router/mainRoute";
+import { Provider } from "react-redux";
+import { store } from "./components/global/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const App = () => {
+  const persistor = persistStore(store);
   return (
     <div>
       <Provider store={store}>
-      <RouterProvider router={mainRoute}/>
-
+        <PersistGate persistor={persistor} loading={null}>
+          <RouterProvider router={mainRoute} />
+        </PersistGate>
       </Provider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
